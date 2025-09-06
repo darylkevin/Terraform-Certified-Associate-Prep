6577ac369b757cfe0040fc64ddf1b1f968946aef
Daryl Kevin <dkevin77@icloud.com>
2025-09-06 22:30:25 +0800

    learn-terraform-get-started-aws completed

--- learn-terraform-get-started-aws/main.tf
+++ learn-terraform-get-started-aws/main.tf
@@ -15,9 +15,26 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
 
   tags = {
-    Name = "learn-terraform"
+    Name = var.instance_name
   }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames = true
 }
--- learn-terraform-get-started-aws/terraform.tf
+++ learn-terraform-get-started-aws/terraform.tf
@@ -1,4 +1,13 @@
 terraform {
+  cloud {
+    organization = "Daryl-Learns-Terraform"
+
+    workspaces {
+      project = "Learn Terraform"
+      name    = "learn-terraform-aws-get-started"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
